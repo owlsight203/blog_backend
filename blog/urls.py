1ddf9c22158b4094a434f72c88777f4a8127f7e1
owlsight203 <drkcrw203@gmail.com>
2026-08-20 17:20:20 +0700

    Fix static routes in vercel.json for WhiteNoise

--- blog/urls.py
+++ blog/urls.py
@@ -40,3 +40,7 @@ from django.conf.urls.static import static
 urlpatterns += [
     # ... the rest of your URLconf goes here ...
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
